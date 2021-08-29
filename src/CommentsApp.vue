a4d1336a7dd9547603adaea0c2900e151e148c63
YunXinZhiCheng <867395636@qq.com>
2021-08-29 23:54:03 +0800

    5-连接后端

--- src/CommentsApp.vue
+++ src/CommentsApp.vue
@@ -39,7 +39,7 @@
         <!-- 回复按钮
           设置子组件传过来的数据
          -->
-        <ReplyBox @submit="addReply($event, comment.id)" />
+        <ReplyBox @submit="addNewComment($event, comment.id)" />
       </div>
     </div>
   </main>
@@ -59,83 +59,41 @@ import avatar3 from './assets/images/avatar3.png'
 import avatar4 from './assets/images/avatar4.jpeg'
 import avatar5 from './assets/images/avatar5.jpeg'
 
-// 导入 ref 函数
-import { ref } from 'vue'
-let rid = ref(4)
+// 导入 ref 与 onMounted 函数
+import { ref, onMounted } from 'vue'
 
 // 数组数据：留言列表
-const comments = ref([
-  {
-    id: 1,
-    user: '陨星之城',
-    avatar: avatar1,
-    time: '2小时之前',
-    content: '如何把之前纯 HTML 项目改装成了 Vue 3.0 的项目？',
-    // 回复
-    replies: [
-      {
-        id: 2,
-        user: '极客开发',
-        avatar: avatar2,
-        time: '2小时之前',
-        content:
-          '赞一个！添加 vue 的 vite 插件,修改 vite 的配置文件,修改目录结构，在 main.js 中挂载了 vue 节点',
-      },
-      {
-        id: 3,
-        user: '蔚蓝多云',
-        avatar: avatar3,
-        time: '2小时之前',
-        content:
-          '然后把 HTML 按结构拆分成了 Vue 的组件,接着把相关的代码剪切到了对应的组件中,最后定义示例数据，来展示和之前页面一样的效果',
-      },
-      // {
-      //   id: 4,
-      //   user: "账号4",
-      //   avatar: avatar4,
-      //   time: "2小时之前",
-      //   content:
-      //     "回答正确",
-      // },
-      // {
-      //   id: 5,
-      //   user: "账号5",
-      //   avatar: avatar5,
-      //   time: "2小时之前",
-      //   content:
-      //     "赞一个！！！",
-      // },
-    ],
-  },
-])
+const comments = ref([])
 
-// 新评论函数
-const constructNewComment = (content) => {
-  return {
-    id: rid.value++,
-    user: '当前用户',
-    avatar: avatar4,
-    content,
-    time: '1秒前',
-  }
-}
-// 事件处理函数,返回新的留言对象
-const addNewComment = (content) => {
-  // 新的留言对象
-  const newComment = constructNewComment(content)
-  // 将其压入到留言列表中
-  comments.value.push(newComment)
+// 定义获取留言列表函数
+async function getAllComments() {
+  const res = await fetch('/api/comments')
+  comments.value = await res.json()
 }
 
-// 事件处理函数,返回新的回复对象
-const addReply = (content, id) => {
-  const reply = constructNewComment(content)
-  let comment = comments.value.find((comment) => comment.id === id)
-  if (comment.replies) {
-    comment.replies.push(reply)
-  } else {
-    comment.replies = [reply]
-  }
+// 使用生命周期方法
+onMounted(() => {
+  getAllComments()
+})
+
+// 定义发表留言和回复函数
+const addNewComment = async (content, replyTo) => {
+  await fetch(`/api/comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content,
+      ...(replyTo && { replyTo }),
+    }),
+  })
+
+  // 新增加完评论后，自动获取新的评论列表
+  // Notion AIP 有延迟，所以在添加完 page 之后，需要过一会才能获取到新的评论列表
+  setTimeout(async () => {
+    await getAllComments()
+  }, 1000)
 }
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 // 导入
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // 导出
 export default defineConfig({
   plugins: [vue()],
-});
+  // 解决跨域问题:使用服务代理
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
